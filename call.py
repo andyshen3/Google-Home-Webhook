94ca409a496d52947632c1289548931ab6a73e7a
andyshen3 <andyshen2882@gmail.com>
2017-07-19 13:51:43 -0700

    Update call.py

--- call.py
+++ call.py
@@ -3,15 +3,15 @@ from twilio import twiml
 from twilio.rest import Client
 
 def handleCall():
-	print(os.environ.get("TWILIO_ACCOUNT_SID"))
-	print(os.environ.get("TWILIO_AUTH_TOKEN"))
-	print(os.environ.get("MY_NUMBER"))
-	print(os.environ.get("TWILIO_NUMBER"))
-	client = Client(os.environ.get("TWILIO_ACCOUNT_SID"), os.environ.get("TWILIO_AUTH_TOKEN"))
+	print(TWILIO_ACCOUNT_SID)
+	print(TWILIO_AUTH_TOKEN)
+	print(MY_NUMBER)
+	print(TWILIO_NUMBER)
+	client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
 	call = client.calls.create(
-		to=os.environ.get("MY_NUMBER"),
-		from_=os.environ.get("TWILIO_NUMBER"),
+		to=MY_NUMBER,
+		from_=TWILIO_NUMBER,
 		url="http://demo.twilio.com/docs/voice.xml")
 
 	print(call.sid)
