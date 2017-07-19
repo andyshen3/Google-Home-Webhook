8ae6ca0fb138f1b4e680d80d2030406776aa1edd
andyshen3 <andyshen2882@gmail.com>
2017-07-19 13:58:20 -0700

    Update send_sms.py

--- send_sms.py
+++ send_sms.py
@@ -2,7 +2,6 @@ import os
 from twilio import twiml
 from twilio.rest import Client
 
-
 def handleCall():
 	client = Client(os.environ.get("TWILIO_ACCOUNT_SID"), os.environ.get("TWILIO_AUTH_TOKEN"))
 
@@ -12,5 +11,3 @@ def handleCall():
 		body="Hello from Python!")
 
 	print(message.sid)
-
-handleCall()
