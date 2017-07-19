fe56b043cc9396e9133bcdee7c750b7da58c1952
andyshen3 <andyshen2882@gmail.com>
2017-07-19 13:56:58 -0700

    Create send_sms.py

---
+++ send_sms.py
@@ -0,0 +1,16 @@
+import os
+from twilio import twiml
+from twilio.rest import Client
+
+
+def handleCall():
+	client = Client(os.environ.get("TWILIO_ACCOUNT_SID"), os.environ.get("TWILIO_AUTH_TOKEN"))
+
+	message = client.messages.create(
+		to=os.environ.get("MY_NUMBER"),
+		from_=os.environ.get("TWILIO_NUMBER"),
+		body="Hello from Python!")
+
+	print(message.sid)
+
+handleCall()
