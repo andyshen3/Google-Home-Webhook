ddb13f81c47c9036fffb8b11b7d8878ad46d002b
andyshen3 <andyshen2882@gmail.com>
2017-07-20 11:36:35 -0700

    Update send_sms.py

--- send_sms.py
+++ send_sms.py
@@ -5,7 +5,10 @@ from twilio.rest import Client
 def handleMessage(data):
 	client = Client(os.environ.get("TWILIO_ACCOUNT_SID"), os.environ.get("TWILIO_AUTH_TOKEN"))
 
-	content = "Case filed by: " + data[0] + "Case information:" + data[1] + "Category:" + data[2]
+	content = "Case filed by: " + data[0] + "\n" + \
+	"Case information: " + data[1] + "\n" + \
+	"Category: " + data[2]
+	
 	message = client.messages.create(
 		to=os.environ.get("MY_NUMBER"),
 		from_=os.environ.get("TWILIO_NUMBER"),
