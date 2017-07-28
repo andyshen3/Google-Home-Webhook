bff2c02f28262a273f5e490c9498ec74d9f978eb
andyshen3 <andyshen2882@gmail.com>
2017-07-28 12:01:03 -0700

    Update call.py

--- call.py
+++ call.py
@@ -1,23 +1,6 @@
 import os
 from twilio import twiml
 from twilio.rest import Client
-from twilio.twiml.voice_response import VoiceResponse, Gather
-
-@app.route("/voice", methods=['GET', 'POST'])
-def voice():
-    """Respond to incoming phone calls with a menu of options"""
-    # Start our TwiML response
-    resp = VoiceResponse()
-
-    # Start our <Gather> verb
-    gather = Gather(num_digits=1, action='/gather')
-    gather.say('To call San Jose IT Services, press 1. To call Japan IT Services, press 2. To call Philippines IT Services, press 3.')
-    resp.append(gather)
-
-    # If the user doesn't select an option, redirect them into a loop
-    resp.redirect('/voice')
-
-    return str(resp)
 
 def handleCall():
 	client = Client(os.environ.get("TWILIO_ACCOUNT_SID"), os.environ.get("TWILIO_AUTH_TOKEN"))
