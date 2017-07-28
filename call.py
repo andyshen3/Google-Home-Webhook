d6f649f87b9e181cb919db4484d6fb5974820d82
andyshen3 <andyshen2882@gmail.com>
2017-07-28 11:44:09 -0700

    Update call.py

--- call.py
+++ call.py
@@ -19,35 +19,6 @@ def voice():
 
     return str(resp)
 
-def gather():
-    """Processes results from the <Gather> prompt in /voice"""
-    # Start our TwiML response
-    resp = VoiceResponse()
-
-    # If Twilio's request to our app included already gathered digits,
-    # process them
-    if 'Digits' in request.values:
-        # Get which digit the caller chose
-        choice = request.values['Digits']
-
-        # <Say> a different message depending on the caller's choice
-        if choice == '1':
-            return handleCall()
-        elif choice == '2':
-            resp.say('You need support. We will help!')
-            return str(resp)
-        else:
-            # If the caller didn't choose 1 or 2, apologize and ask them again
-            resp.say("Sorry, I don't understand that choice.")
-
-    # If the user didn't choose 1 or 2 (or anything), send them back to /voice
-    resp.redirect('/voice')
-
-    return str(resp)
-
-
-
-
 def handleCall():
 	client = Client(os.environ.get("TWILIO_ACCOUNT_SID"), os.environ.get("TWILIO_AUTH_TOKEN"))
 
